e7e87c6936b0f54c5b32fcf862feab52dd7ef59c
sebin07r <100014504+sebin07r@users.noreply.github.com>
2022-07-25 15:44:11 +0530

    commit4

--- Main.py
+++ Main.py
@@ -7,16 +7,16 @@ class Stack:
     def is_empty(self):
         # Write code here
         if len(self.items)==0:
-        return True
-    else:
-        return False
+            return True
+         else:
+            return False
 
     def is_full(self):
         # Write code here
         if len(self.items)==self.size:
-        return True
-    else:
-        return False
+            return True
+        else:
+            return False
 
     def push(self, data):
         if not self.is_full():
